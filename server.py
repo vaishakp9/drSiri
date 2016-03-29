896f4935747599ec1a0c57e3b1f035110dbb0a21
Vaishak Prashanth <vaishakprashanth@vaishak.local>
2016-03-29 23:52:41 +0530

    second lol

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template ,current_app,jsonify,url_for,redirect,request
+import aiml
 import subprocess,requests,json,webbrowser,csv,sys,random
 import codecs
 #chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
@@ -8,7 +9,12 @@ name = ""
 
 app=Flask(__name__,static_url_path="")
 app.debug=True
-
+def respond_ai(ind):
+    k = aiml.Kernel()
+    k.learn("firsttry.aiml")
+    k.setBotPredicate("name", "Chatty")
+    response = k.respond(ind)
+    return response
 @app.route('/',methods=['POST','GET'])
 def index():
 	return ("MANJA")
@@ -99,24 +105,27 @@ def post_analyze():
 	if (max == 1) :
 		json_data = json_data['entities'][0]
 		json_data = json_data.keys()
-		url2 = "http://0.0.0.0:5000/"+json_data[0]
-		print(url2)
-		r2 = requests.get(url2)
-		return r2.text
+		r=respond_ai(json_data[0])
+		#url2 = "http://0.0.0.0:5000/"+json_data[0]
+		#print(url2)
+		#r2 = requests.get(url2)
+		#return r2.text
+		return r
 	elif(max==0) :
 		print("this case")
-		url2 = "http://0.0.0.0:5000/"+str(text)
-		r2 = requests.get(url2)
-		return r2.text
+		#url2 = "http://0.0.0.0:5000/"+str(text)
+		#r2 = requests.get(url2)
+		#return r2.text
+		return respond_ai(str(text))
 	else:
 		json_data = json_data['entities'][0]
 		json_data = json_data.keys()
 		json_data2 = json_data3['entities'][1]
 		json_data2 = json_data2.keys()
-		url2 = "http://0.0.0.0:5000/ffx "+json_data[0]+" "+json_data2[0]
-		print (url2)
-		r2 = requests.get(url2)
-		return r2.text
+		#url2 = "http://0.0.0.0:5000/ffx "+json_data[0]+" "+json_data2[0]
+		#print (url2)
+		#r2 = requests.get(url2)
+		return respond_ai("ffx"+json_data[0]+" "+json_data2[0])
 if __name__ =='__main__' :
 	app.run(host="0.0.0.0",port=1993)
 
